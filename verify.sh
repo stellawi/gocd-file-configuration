6bb933d034b160352ca1df4bffeab20905313774
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-18 17:29:09 +0800

    [Update]verify script to use jq

--- verify.sh
+++ verify.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+pushd /tmp
+curl -O https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz
+tar xf jq-1.6.tar.gz
+export PATH=$(pwd)/linux-amd64:$PATH
+popd
+
 validTime=$(TZ=GMT date +"%r")
-echo $validTime
-validVariable="invalid"
+validVariable=$(cat configuration.json | jq '.environment');
 
 if [ $validVariable == "valid" ];then
   echo "Hey, it is invalid"
