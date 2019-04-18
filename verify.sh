e6b5e4494c299e20978b20ac68c217c64f49bbed
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-18 19:55:38 +0800

    [Add]configuration and sample pipeline

--- verify.sh
+++ verify.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-
+su -
 apk update && apk add jq 
 
 validTime=$(TZ=GMT date +"%r")
-validVariable=$(cat $1 | jq '.environment')
+echo $1
+{
+  validVariable=$(cat $1 | jq '.environment')
+  echo validVariable
+} || {
+  exit 1
+}
+
 
 if [[ $validVariable == "valid" ]];then
   echo "Hey, it is invalid"
