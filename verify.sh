039d59d1c4b3cc6800e090d04d7a880e20712c47
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-18 14:51:38 +0800

    [Update]verify script

--- verify.sh
+++ verify.sh
@@ -4,8 +4,7 @@ validTime=$(TZ=GMT date +"%r")
 echo $validTime
 validVariable="invalid"
 
-if [ validVariable="valid" ]
-  then
-    echo "Hey, it is invalid"
-    exit 1
+if [ $validVariable == "valid" ];then
+  echo "Hey, it is invalid"
+  exit 1
 fi
