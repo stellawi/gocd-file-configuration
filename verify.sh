26c32301b34a2e004315dc8498a18e02b420dfc9
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-18 20:22:56 +0800

    [Verify]jq is working

--- verify.sh
+++ verify.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
-
-pushd /tmp
-wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz
-tar -xvzf jq-1.6.tar.gz
-export PATH=$(pwd)/linux-amd64:$PATH
-popd
-
-validTime=$(TZ=GMT date +"%r")
-validVariable=$(cat $1 | jq '.environment')
+{
+    pushd /tmp
+    wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz
+    tar -xvzf jq-1.6.tar.gz
+    export PATH=$(pwd)/linux-amd64:$PATH
+    popd
+}
+&& {
+    validTime=$(TZ=GMT date +"%r")
+    validVariable=$(cat $1 | jq '.environment')
+}
 jq
 echo $1
 echo $validVariable
 
 if [[ $validVariable == "valid" ]];then
-  echo "Hey, it is invalid"
-  exit 1
+    echo "Hey, it is invalid"
+    exit 1
 fi
