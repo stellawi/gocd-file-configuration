4d1ec196abbdf08a9fe91eae837e2bc3d3070e7a
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-25 23:33:10 +0800

    [Modify]Verify script

--- verify.sh
+++ verify.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-pushd /tmp
-wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz
-tar -xvzf jq-1.6.tar.gz
-export PATH=$(pwd)/linux-amd64:$PATH
-popd
-
 validTime=$(TZ=GMT date +"%r")
-validVariable=$(cat $1 | jq '.environment')
+validVariable=$(cat $1 |  grep "environment" | awk '{print $2}')
 
 jq
 cat $1
