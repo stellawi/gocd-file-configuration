0d9ad9c713f583a4c56e67bae6b315ff918a8ef4
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-26 00:20:04 +0800

    [Fix]verify script

--- verify.sh
+++ verify.sh
@@ -5,7 +5,7 @@ validVariable=$(cat $1 |  grep "environment" | awk '{print $2}')
 echo $validVariable
 
 
-if [ "$validVariable" == "kubernetes" ];then
+if [[ "$validVariable" == "kubernetes" ]];then
     echo "Hey, it is invalid"
 fi
 
