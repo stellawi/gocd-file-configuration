742d52cd53fa5194e3968509cad228672b2d94c9
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-25 23:24:48 +0800

    [Modify]Verify script

--- verify.sh
+++ verify.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 pushd /tmp
-wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz;
-tar -xvzf jq-1.6.tar.gz;
-export PATH=$(pwd)/linux-amd64:$PATH;
+wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz
+tar -xvzf jq-1.6.tar.gz
+export PATH=$(pwd)/linux-amd64:$PATH
 popd
 
 validTime=$(TZ=GMT date +"%r")
-# validVariable=$(cat $1 | jq '.environment')
-validVariable="invalid"
+validVariable=$(cat $1 | jq '.environment')
+
 jq
-echo $1
+cat $1
 echo $validVariable
 
-if [[ $validVariable == "valid" ]];then
+if [[ $validVariable == "kubernetes" ]];then
     echo "Hey, it is invalid"
     exit
 fi
