c67d213a8afd3d39432715efad24ae064417fc9c
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-26 07:16:50 +0800

    [Fixed]If statement as "s1" == s1 doesnot work

--- verify.sh
+++ verify.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 validTime=$(TZ=GMT date +"%r")
-validVariable=$(cat $1 |  grep "environment" | awk '{print $2}')
-
-echo $validVariable
-
+validVariable=$(cat $1 |  grep "environment" | awk '{print $2}' | sed 's/"//g')
 
 if [ $validVariable == "kubernetes" ]
 then
     echo "Hey, it is for kubernetes"
+    exit 1
 else
     echo "Hey, it is for zip"
 fi
