df7ce58e8678d3d8d562f2dde28b7ca7aa53c2c0
Stella Widyasari <stellaw@thoughtworks.com>
2019-04-25 23:33:39 +0800

    [Modify]Verify script

--- verify.sh
+++ verify.sh
@@ -3,10 +3,6 @@
 validTime=$(TZ=GMT date +"%r")
 validVariable=$(cat $1 |  grep "environment" | awk '{print $2}')
 
-jq
-cat $1
-echo $validVariable
-
 if [[ $validVariable == "kubernetes" ]];then
     echo "Hey, it is invalid"
     exit
